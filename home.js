3a247ae688e38bd0da0548de0a67b8cd3a2e2802
sanyam jain <53706744+jssam@users.noreply.github.com>
2020-05-22 01:48:54 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import{Text,StyleSheet} from "react-native";
+const home =()=>{
+    return <Text style = {styles.text} > pickachu pika pi</Text>
+};
+const styles = StyleSheet.create({
+    text:{
+        fontSize:50
+    }
+});
+export default home;
