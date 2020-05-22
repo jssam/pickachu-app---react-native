b57e5b374252ce6878cb1920a90d3a8f3464faaa
sanyam jain <53706744+jssam@users.noreply.github.com>
2020-05-23 01:35:43 +0530

    Add files via upload

---
+++ listofcartoons.js
@@ -0,0 +1,39 @@
+import React from "react";
+import{Text,StyleSheet,FlatList} from "react-native";
+const listofcartoons =()=>{
+   const sam = [
+       {name :"1.picakachu"  },
+       {name :"2.schinchan" },
+       {name :"3.harrypotter" },
+       {name :"4.pokemon" },
+       {name :"5.doremon" },
+       {name :"6.nobita" },
+       {name :"7.hazirumaru" },
+       {name :"8.sanyam" }
+   ];
+   return <FlatList
+   horizontal = {true}
+   showsHorizontalScrollIndicator= {false}
+   keyExtractor = {(ram)=>ram.name}
+   data = {sam}
+   renderItem ={
+       ({item})=>{
+       return <Text style = {styles.text}>{item.name}</Text>
+       }
+   }
+   />
+
+};
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    text:{
+        fontSize:50,
+        marginVertical:20
+    }
+});
+export default listofcartoons;
