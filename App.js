6dd0af3c3c4624e4faedd682b010cdb3b816f195
sanyam jain <53706744+jssam@users.noreply.github.com>
2020-05-26 02:41:35 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import{createStackNavigator} from "react-navigation-stack";
 import home from "./src/screens/home";
 import listofcartoons from "./src/screens/listofcartoons";
 import picscartoons from "./src/screens/picscartoons";
+import cartcounter from "./src/screens/cartcounter";
 
 
 const sanyam = createStackNavigator(
@@ -15,9 +16,13 @@ const sanyam = createStackNavigator(
     },
     Home2 : {
       screen: picscartoons
-    }
+    },
 
-  },
+   Home3: {
+    screen: cartcounter
+  }
+
+},
   {
     intialRouteName: "Home",
     defaultNavigationOptions:{
